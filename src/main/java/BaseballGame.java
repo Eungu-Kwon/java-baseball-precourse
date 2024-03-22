46b1f04a278dd962fc18b73b420e26b5672ec702
zero.1 <zero.1@kakaocorp.com>
2024-03-22 16:57:40 +0900

    feat: add method to find ball
    Loop from 0 to 3 and check if there is the same number in the answer.

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -23,4 +23,24 @@ public class BaseballGame {
 		}
 		return ret;
 	}
+
+	private int getBall(int num) {
+		String numStr = Integer.toString(num);
+		int ret = 0;
+		for (int i = 0; i < 3; ++i) {
+			if (isNumberContains(answer, numStr.charAt((i)))) {
+				ret++;
+			}
+		}
+		return ret;
+	}
+
+	private boolean isNumberContains(String num, char ch) {
+		for (int i = 0; i < num.length(); i++) {
+			if (num.charAt(i) == ch) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
