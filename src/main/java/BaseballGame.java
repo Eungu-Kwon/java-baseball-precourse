7fa47d094c1c26dc542ec7bf784ff12d6920d21c
zero.1 <zero.1@kakaocorp.com>
2024-03-22 16:51:06 +0900

    feat: add method to find strikes
    Loop from 0 to 3 and check if there is the same number in the same position.

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,4 +1,5 @@
 public class BaseballGame {
+	String answer = "000";
 	public void mainloop() {
 		while (true) {
 			// read number
@@ -10,5 +11,16 @@ public class BaseballGame {
 			// if end then break
 			break;
 		}
+	}
+
+	private int getStrike(int num) {
+		String numStr = Integer.toString(num);
+		int ret = 0;
+		for (int i = 0; i < 3; ++i) {
+			if (numStr.charAt(i) == answer.charAt(i)) {
+				ret++;
+			}
+		}
+		return ret;
 	}
 }
