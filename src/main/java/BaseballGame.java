b01b4c73939ba8477843f90a876fcf3d36184e55
zero.1 <zero.1@kakaocorp.com>
2024-03-22 16:14:30 +0900

    style: apply naver convection style

---
+++ src/main/java/BaseballGame.java
@@ -0,0 +1,14 @@
+public class BaseballGame {
+	public void mainloop() {
+		while (true) {
+			// read number
+
+			// check result
+
+			// print result
+
+			// if end then break
+			break;
+		}
+	}
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,12 @@
-class MainGame {
-    public void mainloop(){
-        while(true){
-            // read number
-
-            // check result
-
-            // print result
-
-            // if end then break
-            break;
-        }
-    }
-}
-
 public class Main {
-    public static void main(String[] args){
-        while (true){
-            MainGame mg = new MainGame();
-            mg.mainloop();
+	public static void main(String[] args) {
+		while (true) {
+			MainGame mg = new MainGame();
+			mg.mainloop();
 
-            // read res from user to continue game
-            // if end then break
-            break;
-        }
-    }
+			// read res from user to continue game
+			// if end then break
+			break;
+		}
+	}
 }
